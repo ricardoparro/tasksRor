5dea3e57df4cd0d52dd872a789eba774a96ed81b
Ricardo Parro <ricardo.parro@gmail.com>
2015-09-25 16:24:28 +0200

    added delete method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,4 +14,19 @@ class TasksController < ApplicationController
   def edit
   	@task = Task.find params[:id]
   end
+
+  def update 
+  	@task = Task.find params[:id]
+  	@task.task = params[:task][:task]
+  	if @task.save
+  		redirect_to tasks_path, :notice => 'Your task has successfully been updated.'
+  	else
+  		redirect_to :back, :notice => "There was an error updating your task."
+  	end
+  end 
+
+  def destroy 
+  	Task.destroy params[:id]
+  	redirect_to :back, :notice => "Task has been deleted"
+  end 
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -41,6 +41,25 @@ describe "Tasks", type: :request do
 
       page.should have_content 'updated task'
   	end
+    it "should not update an empty task" do
+      visit tasks_path
+      click_link 'Edit'
+
+      fill_in 'Task', :with => ''
+      click_button 'Update Task'
+      current_path.should == edit_task_path(@task)
+      page.should have_content 'There was an error updating your task.'
+
+    end
   end
+
+  describe "DELETE /tasks" do
+    it "should delete a task" do
+      visit tasks_path
+      find('#task_1').click_link 'Delete'
+      page.should have_content 'Task has been deleted'
+      page.should have_no_content 'go to bed'
+    end 
+end
 end
 
