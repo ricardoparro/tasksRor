308448322103303e176fd58a70b783ecc4b698e8
Ricardo Parro <ricardo.parro@gmail.com>
2015-10-04 18:38:15 +0200

    added spec for creation of new user and auto login into the app

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,13 +10,22 @@ describe "Users", type: :request do
         end
         it "goes to login page" do
             visit tasks_path
-            click_link 'login'
+            click_link 'Log In'
             current_path.should == new_session_path
         end
         it "goes to sign up page" do
             visit tasks_path
             click_link 'Sign Up'
             current_path == new_user_path
+        end
+        it "should create an user" do
+            visit new_user_path
+            fill_in 'user_email', :with => 'test@test.com'
+            fill_in 'user_password', :with => '123'
+            fill_in 'user_password_confirmation', :with => '123'
+            click_button 'Sign Up'
+            current_path.should == root_path
+            page.should have_content 'Logged in as test@test.com.'
         end
     end
 end
